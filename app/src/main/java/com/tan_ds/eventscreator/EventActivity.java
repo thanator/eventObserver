388c48ad32334188937a83d77d7ab01975e405c4
thanator <www.godx@mail.ru>
2017-06-15 13:08:48 +0300

    v0.31 added permission checker

--- app/src/main/java/com/tan_ds/eventscreator/EventActivity.java
+++ app/src/main/java/com/tan_ds/eventscreator/EventActivity.java
@@ -1,9 +1,15 @@
 package com.tan_ds.eventscreator;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -11,10 +17,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
 public class EventActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private EventRecyclerViewAdapter mAdapter;
+    private static final int LOADER_ID = 1, PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,7 @@ public class EventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        checkEventPermission();
 
         mAdapter = new EventRecyclerViewAdapter();
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -36,6 +46,47 @@ public class EventActivity extends AppCompatActivity {
                 startActivity(DoSomeThingWithEventActivity.newIntent(view.getContext()));
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        checkEventPermission();
+
+    }
+
+    private void checkEventPermission(){
+        int permissionResultReader = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALENDAR);
+        int permissionResultWriter = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CALENDAR);
+
+        if (permissionResultReader == PackageManager.PERMISSION_GRANTED &&
+                permissionResultWriter == PackageManager.PERMISSION_GRANTED){
+            getSupportLoaderManager().initLoader(LOADER_ID, null, new EventLoaderCallbacks());
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR},
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private class EventLoaderCallbacks implements LoaderManager.LoaderCallbacks<List<Event>>{
+
+        @Override
+        public Loader<List<Event>> onCreateLoader(int i, Bundle bundle) {
+            return new EventLoader(EventActivity.this);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<List<Event>> loader, List<Event> events) {
+            mAdapter.remakeEvents(events);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<List<Event>> loader) {
+
+        }
     }
 
     @Override
