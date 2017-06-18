b07f0975dc4868441011b05491b796c1bab9e150
thanator <www.godx@mail.ru>
2017-06-19 02:38:34 +0300

    v0.43 small code convention fix

--- app/src/main/java/com/tan_ds/eventscreator/EventViewHolder.java
+++ app/src/main/java/com/tan_ds/eventscreator/EventViewHolder.java
@@ -17,28 +17,23 @@ public class EventViewHolder extends RecyclerView.ViewHolder {
     private TextView mNameEvent, mWhatToDoOnEvent, mWhenToDoBeginEvent, mWhenToDoEndEvent;
     private Context mContext;
 
-
-
-
     public EventViewHolder(View itemView) {
         super(itemView);
+        mContext = itemView.getContext();
         mNameEvent = (TextView) itemView.findViewById(R.id.event_name);
         mWhatToDoOnEvent = (TextView) itemView.findViewById(R.id.what_to_do_event);
         mWhenToDoBeginEvent = (TextView) itemView.findViewById(R.id.when_to_do_begin_event);
         mWhenToDoEndEvent = (TextView) itemView.findViewById(R.id.when_to_do_end_event);
-        mContext = itemView.getContext();
     }
 
     public void bindView (Event event){
-        mNameEvent.setText(event.name);
-        mWhatToDoOnEvent.setText(event.what_to_do);
+        mNameEvent.setText(event.getName());
+        mWhatToDoOnEvent.setText(event.getWhat_to_do());
         Date dateFrom, dateTo;
-        dateFrom = new Date(event.date_from);
-        dateTo = new Date(event.date_to);
+        dateFrom = new Date(event.getDate_from());
+        dateTo = new Date(event.getDate_to());
         DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(mContext);
         mWhenToDoBeginEvent.setText(dateFormat.format(dateFrom));
         mWhenToDoEndEvent.setText(dateFormat.format(dateTo));
-
-
     }
 }
